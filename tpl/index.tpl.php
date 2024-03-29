b5928c04f9137ebe42cb2141b2d90b98de6d4eba
matekovago <dzmwso@hallgato.uni-neumann.hu>
2024-03-29 14:30:11 +0100

    first steps

---
+++ tpl/index.tpl.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="hu">
+    <head>
+        <meta charset="utf-8">
+        <title>Beadando_V1</title>
+    </head>
+    <body>
+        <h1>Megjelenik</h1>
+    </body>
+</html>
